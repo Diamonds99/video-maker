3b3bcdbc6f28de706f46149fb90d3a34397dc755
Victor Henrique S Cabral <victor.cabral@ADM.UNIP.BR>
2020-01-10 22:42:56 -0300

    robo de texto com erros ainda

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-const readline = require('readline-sync')
 
-function start() {
+const robots = {
+    userInput: require('./robots/user-input.js'),
+    text: require('./robots/text.js')
+}
+
+async function start() {
     const content = {}
-
-    content.searchTerm = askAndReturnSearchTerm()
-    content.prefix = askAndReturnPrefix()
-
-    function askAndReturnSearchTerm() {
-        return readline.question('Type a Wikipedia search term: ')
-    }
-
-    function askAndReturnPrefix() {
-        const prefixes = ['Who is', 'What is', 'The history of']
-        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
-        const selectedPrefixText = prefixes[selectedPrefixIndex]
-        
-        return selectedPrefixText
-    }
-
-    console.log(content)
+    
+    content = robots.userInput(content)
+    content = await robots.text(content)
 }
 
 start()
